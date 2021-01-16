228847285065fc39e5f2562a70fe29117a3f56a6
Rustam <rystamrv@gmail.com>
2021-01-16 23:26:39 +0200

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -4,7 +4,7 @@ from shop.bot.config import WEBHOOK_URL
 
 
 bot.remove_webhook()
-# time.sleep(0.5)
-# bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
-bot.polling()
-# app.run()
+time.sleep(0.5)
+bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
+# bot.polling()
+app.run()
